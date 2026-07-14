d6f30f8db105438a8b3e43b17938edf7b4102993
Alberto Geniola <albertogeniola@gmail.com>
2026-07-14 23:22:17 +0200

    Fix image issue

--- ApritiSedano/app/build.gradle.kts
+++ ApritiSedano/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "it.geniola.apritisedano"
         minSdk = 35
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
